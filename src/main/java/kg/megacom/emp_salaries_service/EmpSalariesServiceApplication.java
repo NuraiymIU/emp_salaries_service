1d5cf7d0b8f580be07b4098a8f4c00fdd1f630cd
NURAIYM <nurbekovanuraiym2000@gmail.com>
2021-07-02 18:45:47 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/emp_salaries_service/EmpSalariesServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.emp_salaries_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSalariesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpSalariesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megacom/emp_salaries_service/EmpSalariesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.emp_salaries_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpSalariesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
